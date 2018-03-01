535a6ff0aa07b6245f01819b567c28f649f5ed4b
Ken Huh <ken123777@gmail.com>
2018-03-01 18:01:46 +0900

    Update

--- server.js
+++ server.js
@@ -4,16 +4,23 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send({
-    message: 'hello vanilla'
+  res.render('index', {
+    title: '나만 좋아하고 있었다니..',
+    message: '서운하군요...',
+    user: {
+      name: '허근행'
+    }
   });
 });
 
 var PORT_NUMBER = 8000;
 
 app.listen(PORT_NUMBER, () => {
-  console.log(`Server running at http://127.0.0.1:${PORT_NUMBER}/`);
+  console.log(`Server running at http://localhost:${PORT_NUMBER}/`);
 });
 
 // 1. GET /status-check
